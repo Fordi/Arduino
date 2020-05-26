0c094cd29b401834e9bbcec667df8537784d5af8
Bryan Elliott <fordiman@gmail.com>
2020-05-26 09:41:04 -0400

    Added POV hat support

--- Teensy/NES_To_USB/NES_To_USB.ino
+++ Teensy/NES_To_USB/NES_To_USB.ino
@@ -18,6 +18,34 @@
 #define NES_CLK 2
 #define NES_LCH 3
 #define NES_DAT 4
+/*
+          0
+          UP
+      315    45
+ 270 LT   -1   RT 90
+      225    135
+          DN
+          180
+*/
+int hat_map[] = [
+  /* RLDU */
+  /* 0000 */ -1,
+  /* 0001 */ 0,
+  /* 0010 */ 180,
+  /* 0011 */ -1, // Up + Down = illegal; ignore vertical
+  /* 0100 */ 270,
+  /* 0101 */ 315,
+  /* 0110 */ 225,
+  /* 0111 */ 270, // Up + Down = illegal; ignore vertical
+  /* 1000 */ 90,
+  /* 1001 */ 45,
+  /* 1010 */ 135,
+  /* 1011 */ 90,  // Up + Down = illegal; ignore vertical
+  /* 1100 */ -1, // Left + Right = illegal; ignore horizontal
+  /* 1101 */ 0, // Left + Right = illegal; ignore horizontal
+  /* 1110 */ 180, // Left + Right = illegal; ignore horizontal
+  /* 1111 */ -1 // ignore all
+];
 
 // Pulse a pin (clock or latch)
 void pulse(uint8_t pin) {
@@ -40,9 +68,16 @@ void setup() {
 
 void loop() {
   pulse(NES_LCH);
-  for (int x = 0; x <= 7; x++) { // read in the 8 controller buttons that were latched into the 4021
+  // A, B, Select, Start
+  for (int x = 0; x < 4; x++) { // read in the 4 controller buttons that were latched into the 4021
     Joystick.button(x + 1, digitalRead(NES_DAT));
     pulse(NES_CLK);
   }
+  uint8_t dpad = 0;
+  // Up, Down, Left, Right (`dpad` has bits 4..0 = Right, Left, Down, Up)
+  for (int x = 0; x < 4; x++) { // read in the 4 switches for the d-pad state.
+    bitWrite(dpad, x, !digitalRead(nesData));
+  }
+  Joystick.hat(hat_map[dpad]);
   Joystick.send_now();
 }
